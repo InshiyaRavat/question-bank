bfe08358beffab0dfcc65770c1a03002b5be7557
InshiyaRavat <insravat@gmail.com>
2025-05-30 00:36:07 +0530

    refactor: login register UI enhancemnt

--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -1,26 +1,27 @@
-'use client'
-import { PlanContext } from '@/context/PlanContext'
-import { useRouter } from "next/navigation"
-import { SignUp } from "@clerk/nextjs"
-import { useEffect, useContext } from "react"
+"use client";
+import { PlanContext } from "@/context/PlanContext";
+import { useRouter } from "next/navigation";
+import { SignUp } from "@clerk/nextjs";
+import { useEffect, useContext } from "react";
 
 export default function Page() {
-  const { isSubscribed } = useContext(PlanContext)
-  const router = useRouter()
+  const { isSubscribed } = useContext(PlanContext);
+  const router = useRouter();
 
   useEffect(() => {
     if (isSubscribed) {
-      router.push("/question")
+      router.push("/question");
     }
-  }, [isSubscribed, router])
+  }, [isSubscribed, router]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-200">
-      <div className="rounded-2xl p-8 max-w-md w-full">
-        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Create Your Account ✨</h2>
+    <div className="flex-col items-center justify-center min-h-screen bg-gradient-to-br from-[#001219] via-[#005F73] to-[#0A9396] p-6">
+      <h2 className="text-3xl font-extrabold text-center text-[#EE9B00] mb-6">
+        Create Your Account ✨
+      </h2>
+      <div className="w-full flex justify-center">
         <SignUp />
       </div>
     </div>
-  )
+  );
 }
-
