c14a661de926ee775261585c25c89310e8282853
diyaprajapati <diyabprajapati2005@gmail.com>
2025-08-19 08:20:50 +0530

    mod: role based access

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,6 +1,13 @@
-"use client";
 import { AdminSidebarProvider } from "@/components/Admin-side/AdminSidebar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({ children }) {
+export default async function AdminLayout({ children }) {
+  const { sessionClaims } = await auth();
+  console.log(sessionClaims);
+  // if (sessionClaims.role !== "admin") {
+  //   return redirect("/");
+  // }
+
   return <AdminSidebarProvider>{children}</AdminSidebarProvider>;
 }
